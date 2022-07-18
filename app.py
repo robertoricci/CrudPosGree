fe1202b3ae535d42411edf2bae30e55233e3831b
Roberto Carlos Ricci <roberto.rricci@gmail.com>
2022-07-18 16:10:42 -0300

    ss

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 import streamlit as st
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#quando for subir colocar as variaveis cadastradas no HEROKU
+
+host = os.getenv("HOST")
+dbname = os.getenv("DBNAME")
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
 @st.experimental_singleton
 
-def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
+
+def init_connection(): 
+    return psycopg2.connect(host=host,dbname=dbname, user=user,password=password)
 
 conn = init_connection()
 
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
+
 def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
+     with conn.cursor() as cur:
+         cur.execute(query)
+         return cur.fetchall()
+     
+     
 rows = run_query("SELECT * from clie;")
-
-# Print results.
+    # Print results.
 for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+        st.write(f"{row[0]} has a :{row[1]}:")
